7938498e42c1cc9a0a8b229241310cc92ef28de5
qianchenzhumeng <qianchenzhumeng@live.cn>
2017-05-11 16:31:05 +0800

    Update nodemcu_alexa_milight.ino
    Modify it to control more lights.

--- nodemcu_alexa_milight.ino
+++ nodemcu_alexa_milight.ino
@@ -18,8 +18,10 @@
 #define CE_PIN D2
 #define CSN_PIN D8
 
-void officeLightOn();
-void officeLightOff();
+void bedroomLightOn();
+void bedroomLightOff();
+void livingroomLightOn();
+void livingroomLightOff();
 
 const char* ssid = "****";
 const char* password = "****";
@@ -28,7 +30,8 @@ boolean wifiConnected = false;
 
 UpnpBroadcastResponder upnpBroadcastResponder;
 
-Switch *office = NULL;
+Switch *bedroom = NULL;
+Switch *livingroom = NULL;
 
 RF24 radio(CE_PIN, CSN_PIN);
 PL1167_nRF24 prf(radio);
@@ -37,121 +40,123 @@ OpenMiLight MiLight(mlr);
 
 void setup()
 {
-	Serial.begin(115200);
-	MiLight.set_id(0xB0, 0x2C, 0x8C);
-	MiLight.begin();
-	wifiConnected = connectWifi();
-	if(wifiConnected){
+    Serial.begin(115200);
+    MiLight.begin();
+    wifiConnected = connectWifi();
+    if(wifiConnected){
     upnpBroadcastResponder.beginUdpMulticast();
     
     // Define your switches here. Max 14
     // Format: Alexa invocation name, local port no, on callback, off callback
-    office = new Switch("office lights", 80, officeLightsOn, officeLightsOff);
+    bedroom = new Switch("bedroom light", 80, bedroomLightOn, bedroomLightOff);
+    livingroom = new Switch("living room light", 81, livingroomLightOn, livingroomLightOff);
 
     Serial.println("Adding switches upnp broadcast responder");
-    upnpBroadcastResponder.addDevice(*office);
+    upnpBroadcastResponder.addDevice(*bedroom);
+    upnpBroadcastResponder.addDevice(*livingroom);
   }
 }
 
 void loop()
 {
-	if (mlr.available()) {
-		uint8_t packet[7];
-		size_t packet_length = sizeof(packet);
-		mlr.read(packet, packet_length);
-		printf("\nReceived packet: ");
-		for (int i = 0; i < packet_length; i++) {
-			printf("%02X", packet[i]);
-		}
-	}
+    if (mlr.available()) {
+        uint8_t packet[7];
+        size_t packet_length = sizeof(packet);
+        mlr.read(packet, packet_length);
+        printf("\nReceived packet: ");
+        for (int i = 0; i < packet_length; i++) {
+            printf("%02X", packet[i]);
+        }
+    }
 	
-	if(wifiConnected){
-      upnpBroadcastResponder.serverLoop();
-      
-      office->serverLoop();
-	 }
+    if(wifiConnected){
+        upnpBroadcastResponder.serverLoop();
+        bedroom->serverLoop();
+        livingroom->serverLoop();
+    }
 	
-	if (Serial.available())
+    if (Serial.available())
 	{
-		uint8_t c = Serial.read();
+        uint8_t c = Serial.read();
 		switch (c)
 		{
-		case '1':
-			MiLight.set_pair(1);
-			break;
-		case '2':
-			MiLight.set_color_default(1);
-			break;
-		case '3':
-			MiLight.set_status(1, false);
-			break;
-		case '4':
-			MiLight.set_status(1, true);
-			break;
-		case '5':
-			MiLight.set_color(0);
-			break;
-		case '6':
-			MiLight.set_color(50);
-			break;
-		case '7':
-			MiLight.set_brightness(1);
-			break;
-		case '8':
-			MiLight.set_brightness(50);
-			break;
-		case '9':
-			MiLight.set_brightness(100);
-			break;
-		}
-	}
-
+            case '1':
+                MiLight.set_pair(1);
+                break;
+            case '2':
+                MiLight.set_pair(2);
+                break;
+            case '3':
+                MiLight.set_status(1, true);
+                break;
+            case '4':
+                MiLight.set_status(1, false);
+                break;
+            case '5':
+                MiLight.set_status(2, true);
+                break;
+            case '6':
+                MiLight.set_status(2, false);
+                break;
+            default:break;
+        }
+    }
 }
 
-void officeLightsOn() {
-    Serial.print("Switch 1 turn on ...");
-	MiLight.set_status(1, true);
-	MiLight.set_color_default(1);
+void bedroomLightOn() {
+    Serial.print("Bedroom light turn on ...");
+    MiLight.set_status(1, true);
+    MiLight.set_color_default(1);
 }
 
-void officeLightsOff() {
-    Serial.print("Switch 1 turn off ...");
-	MiLight.set_status(1, false);
+void bedroomLightOff() {
+    Serial.print("Bedroom light turn off ...");
+    MiLight.set_status(1, false);
+}
+
+void livingroomLightOn() {
+    Serial.print("Living room light turn on ...");
+    MiLight.set_status(2, true);
+    MiLight.set_color_default(2);
+}
+
+void livingroomLightOff() {
+    Serial.print("Living room light turn off ...");
+    MiLight.set_status(2, false);
 }
 
 boolean connectWifi(){
-	boolean state = true;
-	int i = 0;
+    boolean state = true;
+    int i = 0;
   
-	WiFi.mode(WIFI_STA);
-	WiFi.begin(ssid, password);
-	Serial.println("");
-	Serial.println("Connecting to WiFi");
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    Serial.println("");
+    Serial.println("Connecting to WiFi");
 
-	// Wait for connection
-	Serial.print("Connecting ...");
-	while (WiFi.status() != WL_CONNECTED) {
-		delay(500);
-		Serial.print(".");
-		if (i > 10){
-			state = false;
-			break;
-		}
-		i++;
+    // Wait for connection
+    Serial.print("Connecting ...");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+        if (i > 10){
+            state = false;
+            break;
+        }
+        i++;
+    }
+  
+    if (state){
+        Serial.println("");
+        Serial.print("Connected to ");
+        Serial.println(ssid);
+        Serial.print("IP address: ");
+        Serial.println(WiFi.localIP());
+    }
+    else {
+        Serial.println("");
+        Serial.println("Connection failed.");
 	}
   
-	if (state){
-		Serial.println("");
-		Serial.print("Connected to ");
-		Serial.println(ssid);
-		Serial.print("IP address: ");
-		Serial.println(WiFi.localIP());
-	}
-	else {
-		Serial.println("");
-		Serial.println("Connection failed.");
-	}
-  
-	return state;
+    return state;
 }
-
